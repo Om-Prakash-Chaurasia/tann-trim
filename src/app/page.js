0ff7531a782646d9fce3168713bc13d6d99cdac4
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-05-31 16:48:22 +0530

    This project is ready to deploy on netlify tann-trim-v-0.1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-"use client"
+"use client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBagShopping,
+  faBookmark,
+  faIndianRupee,
+} from "@fortawesome/free-solid-svg-icons";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
@@ -31,21 +35,44 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className="bg-black text-white">
       <Header />
       <Navbar />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gp-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gp-4 mt-4">
         {products.map((product) => (
-          <div key={product.id} className="border p-4">
-            <img src={product.image} alt={product.title} className="w-full h-64 object-cover" /> {/* check this css, image is breaking*/}
+          <div
+            key={product.id}
+            className="border-x-2 border-y-2 rounded-md  p-4 m-2"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-80 h-80"
+            />
+            <FontAwesomeIcon
+              icon={faBookmark}
+              className="text-gray-500 float-right mt-2 cursor-pointer hover:text-white"
+            />
             <h2 className="text-xl my-2">{product.title}</h2>
-            <p className="text-lg font-bold">{product.price}</p>
-            <p className="text-green-500">50% Off</p>
-            <button className="mt-2 p-2 bg-black text-white w-full">Add to Cart</button>
-            <FontAwesomeIcon icon={faBookmark} className="text-gray-500 float-right mt-2" />
+            <div className="container flex mx-auto justify-between">
+              <div className="flex">
+                <p className="text-xl font-bold">
+                  <FontAwesomeIcon
+                    icon={faIndianRupee}
+                    className="text-white"
+                  />
+                  {product.price}
+                </p>
+                <p className="text-green-500 mx-2 my-2 text-xs">50% Off</p>
+              </div>
+              <FontAwesomeIcon
+                icon={faBagShopping}
+                className="text-2xl hover:text-gray-400 cursor-pointer mr-3"
+              />
+            </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
